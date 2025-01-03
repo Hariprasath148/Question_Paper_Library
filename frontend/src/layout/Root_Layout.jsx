70b8e7e6050b7230e6c19d510864965cee94fa11
Hariprasath148 <janhharigithub@gmail.com>
2025-01-03 20:32:38 +0530

    rotuer set

---
+++ frontend/src/layout/Root_Layout.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom"
+
+export const Root_Layout = () => {
+  return (
+    <Outlet/>
+  )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter , createRoutesFromElements , Route , Routes , RouterProvider } from 'react-router-dom'
+import { Root_Layout } from './layout/Root_Layout';
+import { Home } from './pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const router = createBrowserRouter (
+  createRoutesFromElements (
+    <Route path='/' element={<Root_Layout/>}>
+      <Route index element={<Home/>}/>
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>Hello world</div>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
