f4b40429a0906eed75e07126c3fa3b874568d634
Hariprasath148 <janhharigithub@gmail.com>
2025-01-07 10:41:44 +0530

    login

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,23 @@ import dotenv from "dotenv"
 import authRoute from "./routes/auth.route.js"
 import staffroute from "./routes/staff.route.js"
 import connectDB from "./database/connectDB.js"
+import cors from "cors"
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}));
+app.use(express.urlencoded({
+    extended : true
+}))
 app.use("/api/auth",authRoute);
-app.use("/api/staff",staffroute);
+app.use("/api/staff",staffroute); 
+
 
 app.listen(PORT , ()=>{
     console.log("Server is running in the port :",PORT);
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../style/login.css"
 import gnc_logo from "../assets/images/login/gnc.png"
 import gnc_banner from "../assets/images/login/main-backdrop.png"
+import { useMutation , QueryClient} from '@tanstack/react-query';
 
 export const Login = () => {
+
+  const [formData , setFormData] = useState({
+    staffId : "",
+    password : ""
+  });
+
+  const {mutate : login , isPending , isError , error } = useMutation({
+    mutationFn : async ({staffId , password})=>{
+      try {
+        const res = await fetch("http://localhost:5500/api/auth/login",{
+          method : "POST",
+          credentials : "include",
+          headers : {
+            "Content-Type" : "application/json"
+          },
+          body : JSON.stringify({staffId , password})
+        })
+  
+        const data = await res.json();
+  
+        if(!res.ok){
+          throw new Error(data.error);
+        }
+      } catch (error) {
+        throw error;
+      }
+    },
+    onSuccess : ()=>{
+      console.log("login succesful")
+    }
+  })
+
+  function handlelogin(event){  
+    event.preventDefault();
+    login(formData);
+  }
+
+  const handleInputChange = (e) =>{
+    setFormData({...formData,[e.target.name]:e.target.value});
+  }
+
   return (
     <>
       <div className="login-main-container w-100 vh-100 row m-0">
@@ -13,19 +55,19 @@ export const Login = () => {
             <img id='clg-logo' src={gnc_logo} alt="" />
           </div>
           <div className="header h2 mb-4 text-center">Log in to your account</div>
-          <form action="" className='w-100'>
+          <form action="" className='w-100' onSubmit={handlelogin}>
             <div className="mb-3 form-group">
               <label htmlFor="staffID" className='mb-2'>Enter your staffID :</label>
-              <input type="text" className="form-control mb-2" id="staffId" aria-describedby="staffhelp"/>
+              <input type="text" className="form-control mb-2" id="staffId" name='staffId' onChange={handleInputChange} aria-describedby="staffhelp" autoComplete="username"/>
               <div id='staffhelp' className='form-text'>Example : 234231235412</div>
             </div>
             <div className="mb-2 form-group">
               <label htmlFor="staffpassword" className='mb-2'>Enter your password :</label>
-              <input type="text" className="form-control" id="staffpassword" aria-describedby="staffhelp"/>
+              <input type="password" className="form-control" id="staffpassword"  name='password' onChange={handleInputChange} aria-describedby="staffhelp" autoComplete="current-password"/>
             </div>
-            <div className='error-text mb-3 form-text text-decoration-underline text-danger'>invalid password</div>
+            {isError && <div className='error-text mb-3 form-text text-decoration-underline text-danger'>{error.message}</div>}
             <div className='form-group'>
-              <button type='submit' id='login-btn' className='btn text-center w-25'>Login</button>
+              <button type='submit' id='login-btn' className='btn text-center w-25'>{isPending ? "loading" : "Login"}</button>
             </div>
           </form>
         </div>
