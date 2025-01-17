4a58ea09a654ec46a2eb6752aabd11c6b6dfeee4
Hariprasath148 <janhharigithub@gmail.com>
2025-01-17 21:21:59 +0530

    library

--- frontend/src/components/Staff_Library.jsx
+++ frontend/src/components/Staff_Library.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export const Staff_Library = () => {
   return (
-    <div>Staff_Library</div>
+    <>
+    <div className="container-xxl rounded-4 overflow-hidden p-3 staff-container mt-5" id="staff-card">
+      <div className="card m-0 rounded-0 border-0">
+        <div className="card-header row m-0 p-0 justify-content-start bg-transparent border-0">
+          <div className="col-md-4 col-6 d-flex p-0 ps-2 align-items-center">
+            <div className="vr h-100 side-line rounded opacity-100"></div>
+            <div id="staff-label" className="card-header bg-transparent h5 border-0 fw-normal text-wrap ps-2 p-0">Question Paper Library</div>
+          </div>
+        </div>
+      </div>
+      <div className="card-body p-3"> 
+      </div>  
+    </div>
+  </>
   )
 }
