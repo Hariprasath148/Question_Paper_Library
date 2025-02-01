7e4eda74cc4cabcae998fdc8682cb9bd09fa7cc8
Deepshik11 <deepdeepshik500@gmail.com>
2025-02-01 22:22:48 +0530

    add subject changes

--- backend/controllers/questionpaper.controller.js
+++ backend/controllers/questionpaper.controller.js
@@ -1,4 +1,5 @@
 import uploadToGoogleDrive from "../googledrive/googledriveupload.js";
+import Subject from "../models/subject.model.js"
 
 export const savePDF = async ( req , res ) => {
     try {
@@ -14,3 +15,40 @@ export const savePDF = async ( req , res ) => {
         res.status(500).json({error : "Inter server Error"});
     }
 };
+// Add a new subject
+export const add_subject = async (req, res) => {
+    try {
+        const { Subject_name, Subject_code } = req.body;
+
+        const existingSubject = await Subject.findOne({ Subject_name });
+        if (existingSubject) {
+            return res.status(400).json({ message: "Subject already exists" });
+        }
+
+        const newSubject = new Subject({ Subject_name, Subject_code });
+        await newSubject.save();
+
+        res.status(201).json({ message: "Subject added successfully", subject: newSubject });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+// Get subjects
+
+export const get_subject = async (req, res) => {
+    try {
+        const subjects = await Subject.find({}, { Subject_name: 1, _id: 1 });  // Fetch only required fields
+        
+        // Check if no subjects were found
+        if (subjects.length === 0) {
+            return res.status(404).json({ message: "No subjects found" });
+        }
+
+        res.status(200).json(subjects);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+};
+
+
--- backend/routes/questionpaper.route.js
+++ backend/routes/questionpaper.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { savePDF } from "../controllers/questionpaper.controller.js";
+import { savePDF} from "../controllers/questionpaper.controller.js";
 import upload  from "../middleware/uploadPDF.js";
-
+import { add_subject, get_subject } from '../controllers/questionpaper.controller.js'; 
 const router = express.Router();
 
 
 router.post("/save-question-paper",upload.single("file"),savePDF);
-
+router.post("/add-subject",add_subject)
+router.get("/get-subject",get_subject)
 export default router;
